0c7557d615a11697f0999b9bb83c8eaec3dd5f79
jonathanp350 <mkfjonathan@gmail.com>
2022-11-14 08:13:14 -0300

    Aula

---
+++ Programação Imperativa/Aula_11/index.js
@@ -0,0 +1,13 @@
+
+function somarArray (soma, soma2, soma3) {
+    let total1 = soma[0] + soma[1] + soma[2] + soma[3];
+    
+    return total1;
+}
+
+console.log(somarArray([1,2,3,4]));
+
+
+let filmes = ['VIngadores', 'Vingadores2'];
+console.log(filmes.toUpperCase());
+console.log('alfabeto'.toUpperCase());
