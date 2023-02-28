c27df167292b7d9585140219011cc1343a9d0ae7
jonathansp10 <114629177+jonathansp10@users.noreply.github.com>
2023-02-28 20:55:42 -0300

    Aula05

---
+++ Front-End-2/Aula 05/mesa/main.js
@@ -0,0 +1,11 @@
+function darkMode (){
+    var element = document.body('dark');
+    element.className = "dark";
+}
+
+function lightMode() {
+    var element = document.body;
+    var content = document.getElementById("DarkModetext");
+    element.className = "light";
+    content.innerText = "Dark Mode is OFF";
+  }
