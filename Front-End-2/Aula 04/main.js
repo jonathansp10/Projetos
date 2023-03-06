41f42914ee8f87a4c1c117deece01026af6f4e92
jonathansp10 <114629177+jonathansp10@users.noreply.github.com>
2023-03-06 14:56:30 -0300

    Aula 04

---
+++ Front-End-2/Aula 04/main.js
@@ -0,0 +1,54 @@
+//Obtendo o Body
+document.querySelector('body')
+
+    //Obtendo h1
+    document.querySelector('#TituloPrincipal')
+
+    //Obtendo DIV
+    document.querySelector('.conteudo')
+
+        //Obtendo H5
+        document.querySelector('h5')
+
+        //Obtendo li
+        document.querySelector('#item1')
+        document.querySelector('#item2')
+        document.querySelector('#item3')
+
+    //Obtendo Article
+    document.querySelector('#card1')
+
+        //Obtendo h2
+        document.querySelector('#titleCard1')
+        
+    //Obtendo Article Card2
+    document.querySelector('#card2')
+
+        //Obtendo h2
+        document.querySelector('#titleCard2')
+
+    //Obtendo Article Animado
+    document.querySelector('.animado')
+        
+        //Obtendo h2
+        document.querySelector('cardAnimado')
+     
+    //Obtendo Article Diferente
+    document.querySelector('.diferente')
+
+        //Obtendo h2
+        document.querySelector('#cardDifeterente')
+
+    
+    //Obtendo Article
+    document.querySelector('#card5')
+
+        //Obtendo h2
+        document.querySelector('#titleCard4')
+    
+        
+    //Obtendo Article
+    document.querySelector('#card5')
+
+    //Obtendo h2
+    document.querySelector('#titleCard5')
