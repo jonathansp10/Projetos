697c2bf9000fa792ce50471cdf15c9c62aabaf34
jonathansp10 <114629177+jonathansp10@users.noreply.github.com>
2023-03-12 21:16:47 -0300

    Aula 08

---
+++ Front-End-2/Aula 08/main.js
@@ -0,0 +1,9 @@
+// window.addEventListener()
+
+const testeButtonRef = document.querySelector('#testeButton')
+
+function clickButao() {
+
+    console.log('O botão foi clickado')
+}
+testeButtonRef.addEventListener('click', clickButao)
